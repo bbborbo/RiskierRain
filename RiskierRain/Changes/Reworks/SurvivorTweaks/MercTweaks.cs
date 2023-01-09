79403aa0c8f19831c66b72ea8ee07dc593cf1198
BORBO <bwofodol@gmail.com>
2023-01-08 18:51:18 -0600

    more merc tweaks

--- RiskierRain/Changes/Reworks/SurvivorTweaks/MercTweaks.cs
+++ RiskierRain/Changes/Reworks/SurvivorTweaks/MercTweaks.cs
@@ -16,21 +16,26 @@ namespace RiskierRain.SurvivorTweaks
     class MercTweaks : SurvivorTweakModule
     {
         public override string survivorName => "Mercenary";
-
         public override string bodyName => "MERCBODY";
-        public float secondaryCooldown = 4f; //2.5f
-        public float utilityCooldown = 10f; //8f
-        public float specialCooldown = 8f; //6f
+
+        public float moveSpeed = 8f; //7f
+
+        public float secondaryCooldown = 3f; //2.5f
+        public float utilityCooldown = 11f; //8f
+        public float specialCooldown = 9f; //6f
 
         public override void Init()
         {
             GetBodyObject();
             GetSkillsFromBodyObject(bodyObject);
 
-            //CharacterBody body = bodyObject.GetComponent<CharacterBody>();
+            CharacterBody body = bodyObject.GetComponent<CharacterBody>();
+            body.moveSpeed = moveSpeed;
 
             secondary.variants[0].skillDef.baseRechargeInterval = secondaryCooldown;
+            secondary.variants[0].skillDef.cancelSprintingOnActivation = false;
             secondary.variants[1].skillDef.baseRechargeInterval = secondaryCooldown;
+            secondary.variants[1].skillDef.cancelSprintingOnActivation = false;
 
             utility.variants[0].skillDef.baseRechargeInterval = utilityCooldown;
             utility.variants[1].skillDef.baseRechargeInterval = utilityCooldown;

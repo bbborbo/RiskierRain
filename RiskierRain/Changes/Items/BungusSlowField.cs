5ed44858821b086c96c9407178091f27d4ef38af
BORBO <bwofodol@gmail.com>
2023-01-13 21:02:48 -0600

    slungus!!

--- RiskierRain/Changes/Items/BungusSlowField.cs
+++ RiskierRain/Changes/Items/BungusSlowField.cs
@@ -8,12 +8,15 @@ using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.AddressableAssets;
+using RoR2.Projectile;
 
 namespace RiskierRain.Items
 {
     class Slungus : ItemBase
     {
-        public static BuffDef slungusField;
+        public static GameObject slungusSlowFieldPrefab;
+        public static BuffDef slungusBuff;
         public static float slungusWaitTime = 1f;
 
 
@@ -48,20 +51,34 @@ namespace RiskierRain.Items
             CreateItem();
             CreateLang();
             CreateBuff();
+            CreateSlowField();
             Hooks();
+        }
+
+        private void CreateSlowField()
+        {
+            GameObject railerSlowField = Addressables.LoadAssetAsync<GameObject>("RoR2/DLC1/Railgunner/RailgunnerMineAltDetonated.prefab").WaitForCompletion();
+
+            slungusSlowFieldPrefab = railerSlowField.InstantiateClone("SlungusSlowField");
+
+            //this specifically handles slowing down of projectiles, NOT of characters
+            SlowDownProjectiles slowDownProjectiles = slungusSlowFieldPrefab.GetComponent<SlowDownProjectiles>();
+
+            //this adds the buff to characterbodies inside its radius for slowing down
+            BuffWard buffWard = slungusSlowFieldPrefab.GetComponent<BuffWard>();
         }
 
         void CreateBuff()
         {
-            slungusField = ScriptableObject.CreateInstance<BuffDef>();
+            slungusBuff = ScriptableObject.CreateInstance<BuffDef>();
             {
-                slungusField.name = "slungusField";
-                slungusField.buffColor = new Color(0.8f, 0.4f, 0f);
-                slungusField.canStack = false;
-                slungusField.isDebuff = false;
-                slungusField.iconSprite = RiskierRainPlugin.mainAssetBundle.LoadAsset<Sprite>("Assets/Textures/Icons/Buff/texBuffCobaltShield.png");
+                slungusBuff.name = "slungusField";
+                slungusBuff.buffColor = new Color(0.8f, 0.4f, 0f);
+                slungusBuff.canStack = false;
+                slungusBuff.isDebuff = false;
+                slungusBuff.iconSprite = RiskierRainPlugin.mainAssetBundle.LoadAsset<Sprite>("Assets/Textures/Icons/Buff/texBuffCobaltShield.png");
             };
-            Assets.buffDefs.Add(slungusField);
+            Assets.buffDefs.Add(slungusBuff);
         }
 
         private void AddItemBehavior(On.RoR2.CharacterBody.orig_OnInventoryChanged orig, RoR2.CharacterBody self)
@@ -77,6 +94,7 @@ namespace RiskierRain.Items
 
     public class SlungusItemBehavior : CharacterBody.ItemBehavior
     {
+        public GameObject slungusFieldInstance;
         private void FixedUpdate()
         {
             if (NetworkServer.active)
@@ -85,22 +103,40 @@ namespace RiskierRain.Items
 
                 if (stack > 0 && notMovingStopwatch >= Slungus.slungusWaitTime)
                 {
-                    if (!body.HasBuff(Slungus.slungusField))
+                    if (!body.HasBuff(Slungus.slungusBuff))
                     {
-                        this.body.AddBuff(Slungus.slungusField);
+                        EnableSlungus();
                         return;
                     }
                 }
-                else if (body.HasBuff(Slungus.slungusField))
+                else if (body.HasBuff(Slungus.slungusBuff))
                 {
-                    body.RemoveBuff(Slungus.slungusField);
+                    DisableSlungus();
                 }
+            }
+        }
+
+        private void EnableSlungus()
+        {
+            this.body.AddBuff(Slungus.slungusBuff);
+            if (!slungusFieldInstance)
+            {
+                slungusFieldInstance = Instantiate(Slungus.slungusSlowFieldPrefab);
+            }
+        }
+
+        private void DisableSlungus()
+        {
+            body.RemoveBuff(Slungus.slungusBuff);
+            if (slungusFieldInstance)
+            {
+                Destroy(slungusFieldInstance);
             }
         }
 
         private void OnDisable()
         {
-            this.body.RemoveBuff(Slungus.slungusField);
+            DisableSlungus();
         }
     }
 }

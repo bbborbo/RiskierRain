de5421443a3ed5345f706303b9c9347b834e4f64
BORBO <bwofodol@gmail.com>
2023-02-09 21:16:29 -0600

    solenoid

--- RiskierRain/Changes/Items/VoidLaserTurbine.cs
+++ RiskierRain/Changes/Items/VoidLaserTurbine.cs
@@ -19,7 +19,7 @@ namespace RiskierRain.Items
         public static BuffDef turbineChargeBuff;
         public static BuffDef turbineReadyBuff;
         public static float secondsOfChargeRequired = 60;
-        public override string ItemName => "Super Solonoid Engine"; //Super Solonoid Organ
+        public override string ItemName => "Super Solenoid Engine"; //Super Solonoid Organ
 
         public override string ItemLangTokenName => "VOIDLASERTURBINE";
 

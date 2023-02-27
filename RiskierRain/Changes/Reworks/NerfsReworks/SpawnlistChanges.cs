5b0b8ad69d26d260305ae7dee675cd5a3cd082b4
Orange_Peel <citrus.shell@gmail.com>
2023-02-26 16:52:25 -0800

    removed some boss spawns

--- RiskierRain/Changes/Reworks/NerfsReworks/SpawnlistChanges.cs
+++ RiskierRain/Changes/Reworks/NerfsReworks/SpawnlistChanges.cs
@@ -13,26 +13,47 @@ namespace RiskierRain
 
         public void ChangeSpawnlists()
         {
-           // //get spawncards
-           // SpawnCard verminSnowyCard = LegacyResourcesAPI.Load<SpawnCard>("RoR2/DLC1/Vermin/cscVerminSnowy.asset");
-           // DirectorCardCategorySelection snowyForestDCCS = LegacyResourcesAPI.Load<DirectorCardCategorySelection>("RoR2/DLC1/snowyforest/dccsSnowyForestMonstersDLC1.asset");
-           //
-           // DirectorCard verminSnowyDirectorCard = new DirectorCard
-           // {
-           //     spawnCard = verminSnowyCard,
-           //     selectionWeight = 1000,
-           //     spawnDistance = DirectorCore.MonsterSpawnDistance.Close,
-           //     preventOverhead = true,
-           //     minimumStageCompletions = 0,                
-           // };
+            // //get spawncards
+            // SpawnCard verminSnowyCard = LegacyResourcesAPI.Load<SpawnCard>("RoR2/DLC1/Vermin/cscVerminSnowy.asset");
+            // DirectorCardCategorySelection snowyForestDCCS = LegacyResourcesAPI.Load<DirectorCardCategorySelection>("RoR2/DLC1/snowyforest/dccsSnowyForestMonstersDLC1.asset");
+            //
+            // DirectorCard verminSnowyDirectorCard = new DirectorCard
+            // {
+            //     spawnCard = verminSnowyCard,
+            //     selectionWeight = 1000,
+            //     spawnDistance = DirectorCore.MonsterSpawnDistance.Close,
+            //     preventOverhead = true,
+            //     minimumStageCompletions = 0,                
+            // };
+
+            //roost
+            Helpers.RemoveExistingMonsterFromStage(Helpers.MonsterNames.StoneTitan, DirectorAPI.Stage.DistantRoost);
+
+            //plains
+            Helpers.RemoveExistingMonsterFromStage(Helpers.MonsterNames.BeetleQueen, DirectorAPI.Stage.TitanicPlains);
 
             //siphoned
             Helpers.RemoveExistingMonsterFromStage(Helpers.MonsterNames.LesserWisp, DirectorAPI.Stage.SiphonedForest);
+            Helpers.RemoveExistingMonsterFromStage(Helpers.MonsterNames.WanderingVagrant, DirectorAPI.Stage.SiphonedForest);
             // Helpers.RemoveExistingMonsterFromStage(Helpers.MonsterNames.Beetle, DirectorAPI.Stage.SiphonedForest);
             // Helpers.AddNewMonsterToStage(verminSnowyDirectorCard, MonsterCategory.BasicMonsters, DirectorAPI.Stage.SiphonedForest);
 
+            //wetland
+            Helpers.RemoveExistingMonsterFromStage(Helpers.MonsterNames.StoneTitan, DirectorAPI.Stage.WetlandAspect);
+
+            //sanctuary
+            Helpers.RemoveExistingMonsterFromStage(Helpers.MonsterNames.BeetleQueen, DirectorAPI.Stage.AphelianSanctuary);
+
+            //
+
             //abyssal
             Helpers.RemoveExistingMonsterFromStage(Helpers.MonsterNames.StoneTitanAbyssalDepths, DirectorAPI.Stage.AbyssalDepths);
+
+            //grove
+            Helpers.RemoveExistingMonsterFromStage(Helpers.MonsterNames.StoneTitan, DirectorAPI.Stage.SunderedGrove);
+
+            //Sky meadow
+            Helpers.RemoveExistingMonsterFromStage(Helpers.MonsterNames.XiConstruct, DirectorAPI.Stage.SkyMeadow);
 
 
         }

4a464898c513222004439c484f67ae2a66414c10
Orange_Peel <citrus.shell@gmail.com>
2023-03-20 13:39:12 -0700

    assets time: cloud in a bottle and fart in a jar

--- RiskierRain/Changes/Items/BottleCloud.cs
+++ RiskierRain/Changes/Items/BottleCloud.cs
@@ -33,7 +33,7 @@ namespace RiskierRain.Items
 
         public override BalanceCategory Category => BalanceCategory.StateOfDefenseAndHealing;
 
-        public override GameObject ItemModel => LegacyResourcesAPI.Load<GameObject>("prefabs/NullModel");
+        public override GameObject ItemModel => RiskierRainPlugin.orangeAssetBundle.LoadAsset<GameObject>("Assets/Prefabs/bottle.prefab");
 
         public override Sprite ItemIcon => LegacyResourcesAPI.Load<Sprite>("textures/miscicons/texWIPIcon");
 

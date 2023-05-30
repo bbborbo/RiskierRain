28ce3c55523c600a090a6af1a9e968fc6a2a3b62
Orange_Peel <citrus.shell@gmail.com>
2023-05-29 21:44:32 -0700

    storms that meteor

--- RiskierRain/Changes/Reworks/Mechanical/Storms.cs
+++ RiskierRain/Changes/Reworks/Mechanical/Storms.cs
@@ -3,6 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
+using RoR2;
+using System.Linq;
+using UnityEngine.AddressableAssets;
 
 namespace RiskierRain
 {
@@ -11,10 +14,15 @@ namespace RiskierRain
         public static float drizzleStormDelay = 10;
         public static float rainstormStormDelay = 5;
         public static float monsoonStormDelay = 3;
+
+        //this might suck
+        GameObject teleporter;
+
         void InitializeStorms()
         {
             On.RoR2.Run.BeginStage += StormsBeginStage;
             On.RoR2.Run.EndStage += StormsEndStage;
+            On.RoR2.Run.OnServerTeleporterPlaced += Run_OnServerTeleporterPlaced;
         }
         GameObject StormControllerInstance; //this probably sucks but im just tryna see if itll work lol
         private void StormsBeginStage(On.RoR2.Run.orig_BeginStage orig, RoR2.Run self)
@@ -24,6 +32,9 @@ namespace RiskierRain
                 Destroy(StormControllerInstance);
             StormControllerInstance = new GameObject();
             StageStormController guh = gameObject.AddComponent<StageStormController>();
+            
+            guh.teleporter = teleporter;
+            teleporter = null;
         }
 
         private void StormsEndStage(On.RoR2.Run.orig_EndStage orig, RoR2.Run self)
@@ -32,6 +43,13 @@ namespace RiskierRain
             if (StormControllerInstance == null)
                 return;
             Destroy(StormControllerInstance);
+        }
+        private void Run_OnServerTeleporterPlaced(On.RoR2.Run.orig_OnServerTeleporterPlaced orig, Run self, SceneDirector sceneDirector, GameObject teleporter)
+        {
+            orig.Invoke(self, sceneDirector, teleporter);
+            
+            this.teleporter = teleporter;
+            
         }
     }
 
@@ -52,6 +70,8 @@ namespace RiskierRain
         bool hasSentStormWarning = false;
         bool hasBegunStorm = false;
 
+        public GameObject teleporter;
+
         void Start()
         {
             stageBeginTime = RoR2.Run.instance.GetRunStopwatch();
@@ -62,7 +82,7 @@ namespace RiskierRain
         void CalculateStormStartDelay()
         {
             float delay = 0;
-            switch (RoR2.Run.instance.selectedDifficulty)
+            switch (Run.instance.selectedDifficulty)
             {
                 default:
                     delay = RiskierRainPlugin.monsoonStormDelay;
@@ -74,7 +94,7 @@ namespace RiskierRain
                     delay = RiskierRainPlugin.drizzleStormDelay;
                     break;
             }
-            float random = RoR2.Run.instance.stageRng.RangeFloat(0, 2);
+            float random = Run.instance.stageRng.RangeFloat(0, 2);
             stormStartDelay = (delay + random) * 11;//60; small number for testing thx
         }
         void DetermineStormType()
@@ -88,6 +108,10 @@ namespace RiskierRain
 
         void FixedUpdate()
         {
+            if (Run.instance == null)
+            {
+                Destroy(this.gameObject);
+            }
             float currentTime = RoR2.Run.instance.GetRunStopwatch();
             if (!hasSentStormWarning && currentTime > stormStartTime - 30)
             {
@@ -135,7 +159,63 @@ namespace RiskierRain
 
                 RoR2.Chat.AddMessage(warningMessage);
                 ActivateStorm();
-                
+            }
+
+            if (hasBegunStorm)
+            {
+                StormBehavior();
+            }
+        }
+
+        void StormBehavior()
+        {
+            //thisa is just for meteor stuff; we can make it work for the other storsm when they start existing lol.
+            this.waveTimer -= Time.fixedDeltaTime;
+            if (this.waveTimer <= 0f)
+            {
+                this.waveTimer = UnityEngine.Random.Range(this.waveMinInterval, this.waveMaxInterval);
+                MeteorStormController.MeteorWave item = new MeteorStormController.MeteorWave(CharacterBody.readOnlyInstancesList.ToArray<CharacterBody>(), base.transform.position);
+                this.waveList.Add(item);
+            }
+
+            for (int i = this.waveList.Count - 1; i >= 0; i--)
+            {
+                MeteorStormController.MeteorWave meteorWave = this.waveList[i];
+                meteorWave.timer -= Time.fixedDeltaTime;
+                if (meteorWave.timer <= 0f)
+                {
+                    meteorWave.timer = UnityEngine.Random.Range(0.05f, 1f);
+                    MeteorStormController.Meteor nextMeteor = meteorWave.GetNextMeteor(); // getnextmeteor handles some stuff here, we can look into canibalizing it for more adaptable stuff
+                    if (nextMeteor == null)
+                    {
+                        this.waveList.RemoveAt(i);
+                    }
+                    else if (nextMeteor.valid)
+                    {
+                        this.meteorList.Add(nextMeteor);
+                        EffectManager.SpawnEffect(this.meteorWarningEffectPrefab, new EffectData
+                        {
+                            origin = nextMeteor.impactPosition,
+                            scale = this.meteorbBastRadius
+                        }, true);
+                    }
+                }
+            }
+
+            float num = Run.instance.time - this.meteorImpactDelay;
+            float num2 = num - this.meteorTravelEffectDuration;
+            for (int j = this.meteorList.Count - 1; j >= 0; j--)
+            {
+                MeteorStormController.Meteor meteor = this.meteorList[j];
+                if (meteor.startTime < num2 && !meteor.didTravelEffect)
+                {
+                    this.DoMeteorEffect(meteor);
+                }
+                if (meteor.startTime < num)
+                {
+                    this.meteorList.RemoveAt(j);
+                    this.DetonateMeteor(meteor);
+                }
             }
         }
 
@@ -143,7 +223,68 @@ namespace RiskierRain
         {
             hasBegunStorm = true;
 
-
+            this.meteorList = new List<MeteorStormController.Meteor>();
+            this.waveList = new List<MeteorStormController.MeteorWave>();
         }
+
+
+
+
+        private void DetonateMeteor(MeteorStormController.Meteor meteor)
+        {
+            EffectData effectData = new EffectData
+            {
+                origin = meteor.impactPosition
+            };
+            EffectManager.SpawnEffect(this.meteorImpactEffectPrefab, effectData, true);
+            new BlastAttack
+            {
+                inflictor = base.gameObject,
+                baseDamage = this.meteorBlastDamageCoefficient * 10,//change this flat value to a scaling thing yeagh 
+                baseForce = this.meteorBlastForce,
+                attackerFiltering = AttackerFiltering.AlwaysHit,
+                crit = false,
+                falloffModel = BlastAttack.FalloffModel.SweetSpot,
+                attacker = this.teleporter,
+                bonusForce = Vector3.zero,
+                damageColorIndex = DamageColorIndex.Fragile,
+                position = meteor.impactPosition,
+                procChainMask = default(ProcChainMask),
+                procCoefficient = 0f,
+                teamIndex = TeamIndex.Monster,//this might unintentionally harm void enemies and such but idk how to make something trhat exclusively harms the player team
+                radius = meteorbBastRadius
+            }.Fire();
+        }
+
+        private void DoMeteorEffect(MeteorStormController.Meteor meteor)
+        {
+            meteor.didTravelEffect = true;
+            if (this.meteorImpactEffectPrefab)
+            {
+                EffectManager.SpawnEffect(this.meteorTravelEffectPrefab, new EffectData
+                {
+                    origin = meteor.impactPosition
+                }, true);
+            }
+        }
+
+        //all the projectile/prefab stuff
+        public float waveMinInterval = 1;
+        public float waveMaxInterval = 4;
+
+        private List<MeteorStormController.Meteor> meteorList;
+        private List<MeteorStormController.MeteorWave> waveList;
+        private float waveTimer;
+
+        //meteors:
+        GameObject meteorTravelEffectPrefab;
+        GameObject meteorWarningEffectPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Meteor/MeteorStrikePredictionEffect.prefab").WaitForCompletion();
+        GameObject meteorImpactEffectPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Meteor/MeteorStrikeImpact.prefab").WaitForCompletion();
+        public float meteorTravelEffectDuration = 3;
+        public float meteorImpactDelay = 3;
+        public float meteorBlastDamageCoefficient = 5;
+        public float meteorbBastRadius = 10;
+        public float meteorBlastForce = 0;
+
     }
 }

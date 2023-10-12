405f8dc34325df4ccf84b23a717742dd937fed21
BORBO <bwofodol@gmail.com>
2023-10-11 23:00:14 -0500

    standalone chill rework

---
+++ ChillRework/ChillReworkPlugin.cs
@@ -0,0 +1,78 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using R2API;
+using R2API.Utils;
+using System.Security.Permissions;
+using UnityEngine;
+using static R2API.DamageAPI;
+
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 // Type or member is obsolete
+[module: System.Security.UnverifiableCode]
+#pragma warning disable 
+namespace ChillRework
+{
+    [BepInPlugin(guid, teamName, modName)]
+    [R2APISubmoduleDependency(nameof(LanguageAPI), nameof(ContentAddition), nameof(DamageAPI))]
+    public partial class ChillReworkPlugin : BaseUnityPlugin
+    {
+        #region plugin info
+        public static PluginInfo PInfo { get; private set; }
+        public const string guid = "com." + teamName + "." + modName;
+        public const string teamName = "HouseOfFruits";
+        public const string modName = "ChillRework";
+        public const string version = "1.0.0";
+        #endregion
+
+        public static ModdedDamageType ChillOnHit;
+        public const string chillKeywordToken = "2R4R_KEYWORD_CHILL";
+        public const int chillStacksMax = 10;
+        public const int chillStacksOnFreeze = 3;
+        public const float chillProcDuration = 8f;
+        public const int chillProcChance = 100;
+        public void Awake()
+        {
+            Debug.Log("Chill Rework initializing!");
+            CreateIceNovaAssets();
+            LangFixes();
+            FixSnapfreeze();
+        }
+
+        public void ChillRework()
+        {
+            ChillOnHit = ReserveDamageType();
+            ChillHooks();
+        }
+        public void LangFixes()
+        {
+            //keywords
+            LanguageAPI.Add(chillKeywordToken, "<style=cKeywordName>Chilling</style>" +
+                $"<style=cSub>Has a chance to temporarily <style=cIsUtility>slow enemy speed</style> by <style=cIsDamage>80%.</style></style>");
+            LanguageAPI.Add("KEYWORD_FREEZING",
+                "<style=cKeywordName>Freezing</style>" +
+                "<style=cSub>Freeze enemies in place and <style=cIsUtility>Chill</style> them, slowing them by 80% after they thaw. " +
+                "Frozen enemies are <style=cIsHealth>instantly killed</style> if below <style=cIsHealth>30%</style> health.");
+
+            //items
+            LanguageAPI.Add("ITEM_ICERING_DESC",
+                $"Hits that deal <style=cIsDamage>more than 400% damage</style> also blasts enemies with a " +
+                $"<style=cIsDamage>runic ice blast</style>, " +
+                $"<style=cIsUtility>Chilling</style> them for <style=cIsUtility>3s</style> <style=cStack>(+3s per stack)</style> and " +
+                $"dealing <style=cIsDamage>250%</style> <style=cStack>(+250% per stack)</style> TOTAL damage. " +
+                $"Recharges every <style=cIsUtility>10</style> seconds.");
+            LanguageAPI.Add("ITEM_ICICLE_DESC",
+                   "Killing an enemy surrounds you with an <style=cIsDamage>ice storm</style> " +
+                   "that deals <style=cIsDamage>600% damage per second</style> and " +
+                   "<style=cIsUtility>Chills</style> enemies for <style=cIsUtility>1.5s</style>. " +
+                   "The storm <style=cIsDamage>grows with every kill</style>, " +
+                   "increasing its radius by <style=cIsDamage>1m</style>. " +
+                   "Stacks up to <style=cIsDamage>6m</style> <style=cStack>(+6m per stack)</style>.");
+
+            //skills
+            LanguageAPI.Add("MAGE_UTILITY_ICE_DESCRIPTION",
+                "<style=cIsUtility>Freezing</style>. Create a barrier that hurts enemies for " +
+                "up to <style=cIsDamage>12x100% damage</style>.");
+        }
+    }
+}
---
+++ ChillRework/Hooks.cs
@@ -0,0 +1,95 @@
+﻿using BepInEx;
+using R2API;
+using RoR2;
+using RoR2.Projectile;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using static R2API.DamageAPI;
+
+namespace ChillRework
+{
+    public partial class ChillReworkPlugin : BaseUnityPlugin
+    {
+        public void FixSnapfreeze()
+        {
+            GameObject iceWallPillarPrefab = RoR2.LegacyResourcesAPI.Load<GameObject>("prefabs/projectiles/MageIcewallPillarProjectile");
+            ProjectileImpactExplosion pie = iceWallPillarPrefab.GetComponentInChildren<ProjectileImpactExplosion>();
+            if (pie)
+            {
+                pie.destroyOnEnemy = false;
+            }
+        }
+
+        #region chill on hit
+        private readonly Dictionary<GameObject, GameObject> frozenBy = new Dictionary<GameObject, GameObject>();
+        /// <summary>
+        /// damageInfo, victim
+        /// </summary>
+        public static event Action<DamageInfo, GameObject> OnMaxChill;
+        public void ChillHooks()
+        {
+            On.RoR2.GlobalEventManager.OnHitEnemy += ChillOnHitHook;
+            On.RoR2.CharacterBody.AddBuff_BuffIndex += MaxChillStacks;
+        }
+
+        private void ChillOnHitHook(On.RoR2.GlobalEventManager.orig_OnHitEnemy orig, GlobalEventManager self, DamageInfo damageInfo, GameObject victim)
+        {
+            CharacterBody vBody = victim?.GetComponent<CharacterBody>();
+            CharacterBody aBody = damageInfo.attacker?.GetComponent<CharacterBody>();
+
+            if (vBody != null && aBody != null && damageInfo.procCoefficient != 0 && !damageInfo.rejected)
+            {
+                if (damageInfo.damageType.HasFlag(DamageType.Freeze2s))
+                {
+                    this.frozenBy[victim] = damageInfo.attacker;
+                    float chillCount = chillStacksOnFreeze;
+                    if (damageInfo.damageType.HasFlag(DamageType.AOE))
+                    {
+                        chillCount -= 1;
+                    }
+                    for (int i = 0; i < chillCount; i++)
+                    {
+                        if (Util.CheckRoll(damageInfo.procCoefficient * 100, aBody.master))
+                        {
+                            vBody.AddTimedBuffAuthority(RoR2Content.Buffs.Slow80.buffIndex, chillProcDuration);
+                        }
+                    }
+                }
+                else
+                {
+
+                    if (damageInfo.HasModdedDamageType(ChillOnHit))//(damageInfo.damageType.HasFlag(DamageType.SlowOnHit))
+                    {
+                        damageInfo.RemoveModdedDamageType(ChillOnHit);
+                        float procChance = Mathf.Min(1, chillProcChance * damageInfo.procCoefficient * damageInfo.procCoefficient) * 100;
+
+                        if (Util.CheckRoll(procChance, aBody.master))
+                        {
+                            vBody.AddTimedBuffAuthority(RoR2Content.Buffs.Slow80.buffIndex, chillProcDuration);
+                        }
+                    }
+                }
+            }
+            int chillDebuffCount = vBody.GetBuffCount(RoR2Content.Buffs.Slow80);
+            if (chillDebuffCount >= chillStacksMax) //Arctic Blast
+            {
+                OnMaxChill?.Invoke(damageInfo, victim);
+                /*vBody.ClearTimedBuffs(RoR2Content.Buffs.Slow80);
+                AltArtiPassive.DoNova(aBody, icePower, damageInfo.position, AltArtiPassive.novaDebuffThreshold);*/
+            }
+            orig(self, damageInfo, victim);
+        }
+
+        private void MaxChillStacks(On.RoR2.CharacterBody.orig_AddBuff_BuffIndex orig, CharacterBody self, BuffIndex buffType)
+        {
+            if (buffType == RoR2Content.Buffs.Slow80.buffIndex && self.GetBuffCount(RoR2Content.Buffs.Slow80.buffIndex) >= chillStacksMax)
+            {
+                return;
+            }
+            orig(self, buffType);
+        }
+        #endregion
+    }
+}

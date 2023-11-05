491b5556484b9a19f5241adf64050fccdacde46c
BORBO <bwofodol@gmail.com>
2023-11-05 00:58:14 -0500

    forgot plasma shrimp

--- RiskierRainContent/Changes/Items/Reworks/AtgMissileMk3.cs
+++ RiskierRainContent/Changes/Items/Reworks/AtgMissileMk3.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using static R2API.RecalculateStatsAPI;
 
 namespace RiskierRain.Items
 {
@@ -85,6 +86,7 @@ namespace RiskierRain.Items
             IL.RoR2.GlobalEventManager.OnHitEnemy += RemoveVanillaAtgLogic;
             On.RoR2.BodyCatalog.Init += GetDisplayRules;
             On.RoR2.Items.ContagiousItemManager.Init += ChangeVoidShrimpPairing;
+            ReworkPlasmaShrimp();
         }
 
         private void ChangeVoidShrimpPairing(On.RoR2.Items.ContagiousItemManager.orig_Init orig)
@@ -248,6 +250,77 @@ namespace RiskierRain.Items
         }
         #endregion
 
+        #region plasma shrimp
+        float shrimpShieldBase = 40;
+        float shrimpDamageCoeffBase = 0.09f;
+        float shrimpDamageCoeffStack = 0.04f;
+        public void ReworkPlasmaShrimp()
+        {
+            IL.RoR2.GlobalEventManager.OnHitEnemy += ShrimpRework;
+            GetStatCoefficients += ShrimpShieldFix;
+
+            LanguageAPI.Add("ITEM_MISSILEVOID_PICKUP", "While you have shield, fire missiles on every hit. <style=cIsVoid>Corrupts all AtG Missile Mk. 3s</style>.");
+            LanguageAPI.Add("ITEM_MISSILEVOID_DESC",
+                $"Gain <style=cIsHealing>{shrimpShieldBase} shield</style>. " +
+                $"While you have a <style=cIsHealing>shield</style>, " +
+                $"hitting an enemy fires <style=cIsDamage>3</style> missiles that each deal " +
+                $"<style=cIsDamage>{Tools.ConvertDecimal(shrimpDamageCoeffBase)}</style> " +
+                $"<style=cStack>(+{Tools.ConvertDecimal(shrimpDamageCoeffStack)} per stack)</style> TOTAL damage. " +
+                $"<style=cIsVoid>Corrupts all AtG Missile Mk. 3s</style>.");
+        }
+
+        private void ShrimpShieldFix(CharacterBody sender, StatHookEventArgs args)
+        {
+            Inventory inv = sender.inventory;
+            if (inv && inv.GetItemCount(DLC1Content.Items.MissileVoid) > 0)
+            {
+                args.shieldMultAdd -= 0.1f;
+                args.baseShieldAdd += shrimpShieldBase;
+            }
+        }
+
+        private void ShrimpRework(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            //jump to shrimp implementation
+            int shrimpLoc = 32;
+            c.GotoNext(MoveType.After,
+                x => x.MatchLdsfld("RoR2.DLC1Content/Items", "MissileVoid"),
+                x => x.MatchCallOrCallvirt<RoR2.Inventory>(nameof(RoR2.Inventory.GetItemCount)),
+                x => x.MatchStloc(out shrimpLoc)
+                );
+
+            /*int dmgLoc = 37;
+            c.GotoNext(MoveType.Before,
+                x => x.MatchLdcR4(out _),
+                x => x.MatchLdloc(shrimpLoc),
+                x => x.MatchConvR4(),
+                x => x.MatchMul(),
+                x => x.MatchStloc(out dmgLoc)
+                );*/
+
+            //inject our new damage coefficient
+            c.GotoNext(MoveType.Before,
+                x => x.MatchCallOrCallvirt("RoR2.Util", nameof(RoR2.Util.OnHitProcDamage))
+                );
+            c.Emit(OpCodes.Ldloc, shrimpLoc);
+            c.EmitDelegate<Func<float, int, float>>((damageCoefficient, itemCount) =>
+            {
+                float damageOut = shrimpDamageCoeffBase + (shrimpDamageCoeffStack * (itemCount - 1));
+                return damageOut;
+            });
+
+            //force it to fire 3
+            c.GotoNext(MoveType.Before,
+                x => x.MatchLdloc(out _),
+                x => x.MatchLdcI4(out _),
+                x => x.MatchBgt(out _)
+                );
+            c.Remove();
+            c.Emit(OpCodes.Ldc_I4, 1);
+        }
+        #endregion
     }
     public class Mk3MissileBehavior : RoR2.CharacterBody.ItemBehavior
     {

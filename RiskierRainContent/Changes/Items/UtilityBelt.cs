809aac0521c241b24fa08312e05b07f1a8d14d5c
BORBO <bwofodol@gmail.com>
2023-12-03 00:49:24 -0600

    utility knife description

--- RiskierRainContent/Changes/Items/UtilityBelt.cs
+++ RiskierRainContent/Changes/Items/UtilityBelt.cs
@@ -12,22 +12,23 @@ namespace RiskierRainContent.Items
 {
     class UtilityBelt : ItemBase<UtilityBelt>
     {
-        public static List<string> blacklistedSkillNameTokens = new List<string>(1) { "MAGE_UTILITY_ICE_NAME", "ENGI_SKILL_HARPOON_NAME" };
+        public static List<string> blacklistedSkillNameTokens = new List<string>(1) { "MAGE_UTILITY_ICE_NAME", "ENGI_SKILL_HARPOON_NAME", "CAPTAIN_UTILITY_NAME", "CAPTAIN_UTILITY_ALT_NAME" };
         public static BuffDef utilityBeltCooldown;
-        public static float idealBaseCooldown = 6f;
+        static float minBaseCooldown = 2f;
+        static float maxBaseCooldown = 15f;
 
-        public static float castBarrierBase = 0.15f;
-        public static float castBarrierStack = 0.05f;
+        public static float castBarrierBase = 0.02f;
+        public static float castBarrierStack = 0.005f;
         public override string ItemName => "Utility Knife";
 
-        public override string ItemLangTokenName => "BORBOBARRIERBELT";
+        public override string ItemLangTokenName => "UTILITYBARRIER";
 
         public override string ItemPickupDesc => "Casting your Utility skill grants a temporary barrier.";
 
         public override string ItemFullDescription => $"Casting your Utility skill grants you <style=cIsHealing>a temporary barrier</style> " +
-            $"for <style=cIsHealing>{Tools.ConvertDecimal(castBarrierBase)} health</style> " +
-            $"<style=cStack>(+{Tools.ConvertDecimal(castBarrierStack)} per stack)</style>. " +
-            $"<style=cIsUtility>Affected by Utility cooldown length</style>.";
+            $"for <style=cIsHealing>{Tools.ConvertDecimal(castBarrierBase)}</style> of your maximum health " +
+            $"<style=cStack>(+{Tools.ConvertDecimal(castBarrierStack)} per stack)</style> " +
+            $"per second of your Utility skill's <style=cIsUtility>base cooldown</style>.";
 
         public override string ItemLore => "";
 
@@ -36,7 +37,7 @@ namespace RiskierRainContent.Items
         public override GameObject ItemModel => Assets.orangeAssetBundle.LoadAsset<GameObject>("Assets/Prefabs/mdlUtilityBelt.prefab");
 
         public override Sprite ItemIcon => Assets.orangeAssetBundle.LoadAsset<Sprite>("Assets/Icons/texIconPickupITEM_BORBOBARRIERBELT.png");
-        public override ItemTag[] ItemTags => new ItemTag[] { ItemTag.Healing , ItemTag.Utility };
+        public override ItemTag[] ItemTags => new ItemTag[] { ItemTag.Healing };
 
         public override ItemDisplayRuleDict CreateItemDisplayRules()
         {
@@ -48,6 +49,7 @@ namespace RiskierRainContent.Items
             On.RoR2.CharacterBody.OnSkillActivated += UtilityBeltBarrierGrant;
             //hard compat
             On.EntityStates.Mage.Weapon.PrepWall.OnExit += PrepWall_OnExit;
+            On.EntityStates.Captain.Weapon.CallAirstrikeBase.OnEnter += CallAirstrikeBase_OnEnter;
             On.EntityStates.Engi.EngiMissilePainter.Fire.FireMissile += Fire_FireMissile;
         }
 
@@ -57,7 +59,7 @@ namespace RiskierRainContent.Items
 
             if (!blacklistedSkillNameTokens.Contains(skill.skillNameToken) && skill == self.skillLocator.utility)
             {
-                UtilityBelt.GiveUtilityBarrier(self, skill);
+                UtilityBelt.GiveUtilityBarrier(self, skill.baseRechargeInterval);
             }
         }
 
@@ -66,6 +68,16 @@ namespace RiskierRainContent.Items
         {
             orig(self, target, position);
             UtilityBelt.GiveUtilityBarrier(self.characterBody, self.activatorSkillSlot);
+        }
+
+        private void CallAirstrikeBase_OnEnter(On.EntityStates.Captain.Weapon.CallAirstrikeBase.orig_OnEnter orig, EntityStates.Captain.Weapon.CallAirstrikeBase self)
+        {
+            orig(self);
+            SkillLocator skillLocator = self.GetComponent<SkillLocator>();
+            if (skillLocator)
+            {
+                GiveUtilityBarrier(self.characterBody, skillLocator.utility.baseRechargeInterval / 3);
+            }
         }
 
         private void PrepWall_OnExit(On.EntityStates.Mage.Weapon.PrepWall.orig_OnExit orig, EntityStates.Mage.Weapon.PrepWall self)
@@ -88,6 +100,11 @@ namespace RiskierRainContent.Items
         #region grant barrier
         public static void GiveUtilityBarrier(CharacterBody body, GenericSkill skill)
         {
+            if (skill != null)
+                GiveUtilityBarrier(body, skill.baseRechargeInterval);
+        }
+        public static void GiveUtilityBarrier(CharacterBody body, float skillBaseCooldown)
+        {
             if (body.healthComponent)
             {
                 //body is nullchecked by getcount automatically
@@ -96,10 +113,10 @@ namespace RiskierRainContent.Items
                 if (itemCount > 0f)
                 {
                     float barrierFraction = castBarrierBase + castBarrierStack * (itemCount - 1);
-                    float adjustedBarrier = (body.healthComponent.fullCombinedHealth * barrierFraction) * Mathf.Min(skill.baseRechargeInterval / idealBaseCooldown, 3);
+                    float scaledBarrierFraction = barrierFraction * Mathf.Clamp(skillBaseCooldown, minBaseCooldown, maxBaseCooldown);
                     // float barrier = castBarrierBase + castBarrierStack * (itemCount - 1);
                     // int adjustedBarrier = (int)(barrier * Mathf.Pow(baseCooldown / 2f, 0.75f));
-                    body.healthComponent.AddBarrier(adjustedBarrier);
+                    body.healthComponent.AddBarrier(body.healthComponent.fullCombinedHealth * scaledBarrierFraction);
                 }
             }
         }

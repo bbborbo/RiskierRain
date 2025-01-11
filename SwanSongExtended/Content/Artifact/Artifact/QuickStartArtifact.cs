05d2c60d61e64a866395f62a424cc655d2c3a6ba
BORBO <bwofodol@gmail.com>
2025-01-11 15:56:52 -0600

    ???????????????????????????????

---
+++ SwanSongExtended/Content/Artifact/Artifact/QuickStartArtifact.cs
@@ -0,0 +1,95 @@
+﻿using BepInEx.Configuration;
+using SwanSongExtended.Equipment;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using SwanSongExtended.Modules;
+
+namespace SwanSongExtended.Artifacts
+{
+    class QuickStartArtifact : ArtifactBase<QuickStartArtifact>
+    {
+        #region config
+        public override string ConfigName => "Artifacts : Initiative";
+
+        [AutoConfig("Wish Pickup Index", "2 is Common, 3 is Uncommon, 4 is Rare", 3)]
+        public static int wishPickupIndex = 3;
+        public static string ConvertPickupIndexToRarityName(int n)
+        {
+            string rarityName = "";
+            switch (n)
+            {
+                case 2:
+                    rarityName = "Common";
+                    break;
+                case 3:
+                    rarityName = "Uncommon";
+                    break;
+                case 4:
+                    rarityName = "Rare";
+                    break;
+                default:
+                    break;
+            }
+            return rarityName;
+        }
+        public static string GetRarityName()
+        {
+            return ConvertPickupIndexToRarityName(wishPickupIndex);
+        }
+        #endregion
+
+        public override string ArtifactName => "Initiative";
+
+        public override string ArtifactDescription => $"Begin your run with an {GetRarityName()} item of your choice.";
+
+        public override string ArtifactLangTokenName => "QUICKSTART";
+
+        public override Sprite ArtifactSelectedIcon => LegacyResourcesAPI.Load<Sprite>("textures/miscicons/texWIPIcon");
+
+        public override Sprite ArtifactDeselectedIcon => LegacyResourcesAPI.Load<Sprite>("textures/miscicons/texWIPIcon");
+
+        public override AssetBundle assetBundle => null;
+
+        public override void Hooks()
+        {
+
+        }
+
+        public override void OnArtifactEnabledServer()
+        {
+            On.RoR2.CharacterBody.Start += GiveQuickStart;
+        }
+
+        public override void OnArtifactDisabledServer()
+        {
+            On.RoR2.CharacterBody.Start -= GiveQuickStart;
+        }
+
+        private void GiveQuickStart(On.RoR2.CharacterBody.orig_Start orig, CharacterBody self)
+        {
+            orig(self);
+            bool isStageone = Run.instance.stageClearCount == 0;
+            if (!isStageone)
+            {
+                return;
+            }
+            if (self.isPlayerControlled)
+            {
+                OnPlayerCharacterBodyStartServer(self);
+            }
+        }
+        private static void OnPlayerCharacterBodyStartServer(CharacterBody characterBody)
+        {
+            Inventory inventory = characterBody.inventory;
+            if (inventory != null)
+            {
+                //inventory.SetEquipmentIndex(QuickStartEquipment.instance.EquipDef.equipmentIndex);
+                EquipmentState equipmentState = new EquipmentState(QuickStartEquipment.instance.EquipDef.equipmentIndex, Run.FixedTimeStamp.negativeInfinity, 1);
+                inventory.SetEquipment(equipmentState, 0);
+            }
+        }
+    }
+}
---
+++ SwanSongExtended/Content/Artifact/ArtifactBase.cs
@@ -0,0 +1,90 @@
+﻿using BepInEx.Configuration;
+using R2API;
+using RoR2;
+using SwanSongExtended.Modules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace SwanSongExtended.Artifacts
+{
+    // The directly below is entirely from TILER2 API (by ThinkInvis) specifically the Item module. Utilized to keep instance checking functionality as I migrate off TILER2.
+    // TILER2 API can be found at the following places:
+    // https://github.com/ThinkInvis/RoR2-TILER2
+    // https://thunderstore.io/package/ThinkInvis/TILER2/
+
+    public abstract class ArtifactBase<T> : ArtifactBase where T : ArtifactBase<T>
+    {
+        public static T instance { get; private set; }
+
+        public ArtifactBase()
+        {
+            if (instance != null) throw new InvalidOperationException("Singleton class \"" + typeof(T).Name + "\" inheriting ItemBoilerplate/Item was instantiated twice");
+            instance = this as T;
+        }
+    }
+
+    public abstract class ArtifactBase : SharedBase
+    {
+        public abstract string ArtifactName { get; }
+        public abstract string ArtifactDescription { get; }
+        public abstract string ArtifactLangTokenName { get; }
+        public abstract Sprite ArtifactSelectedIcon { get; }
+        public abstract Sprite ArtifactDeselectedIcon { get; }
+
+        public ArtifactDef ArtifactDef;
+        public abstract void OnArtifactEnabledServer();
+        public abstract void OnArtifactDisabledServer();
+
+        public override void Init()
+        {
+            base.Init();
+            CreateArtifact();
+        }
+
+        public override void Lang()
+        {
+            LanguageAPI.Add("ARTIFACT_" + ArtifactLangTokenName + "_NAME", "Artifact of " + ArtifactName);
+            LanguageAPI.Add("ARTIFACT_" + ArtifactLangTokenName + "_DESCRIPTION", ArtifactDescription);
+        }
+
+        protected void CreateArtifact()
+        {
+            ArtifactDef = ScriptableObject.CreateInstance<ArtifactDef>();
+            {
+                ArtifactDef.cachedName = "2r4r" + ArtifactName;
+                ArtifactDef.nameToken = "ARTIFACT_" + ArtifactLangTokenName + "_NAME";
+                ArtifactDef.descriptionToken = "ARTIFACT_" + ArtifactLangTokenName + "_DESCRIPTION";
+                ArtifactDef.smallIconDeselectedSprite = ArtifactDeselectedIcon;
+                ArtifactDef.smallIconSelectedSprite = ArtifactSelectedIcon;
+                //ArtifactDef.unlockableDef = UnlockableCatalog.GetUnlockableDef("SuicideHermitCrabs");
+            }
+            Content.AddArtifactDef(ArtifactDef);
+
+            RunArtifactManager.onArtifactEnabledGlobal += OnArtifactEnabled;
+            RunArtifactManager.onArtifactDisabledGlobal += OnArtifactDisabled;
+        }
+
+        private void OnArtifactEnabled(RunArtifactManager runArtifactManager, ArtifactDef artifactDef)
+        {
+            if (artifactDef != ArtifactDef)
+                return;
+            if (NetworkServer.active)
+                OnArtifactEnabledServer();
+        }
+
+        private void OnArtifactDisabled(RunArtifactManager runArtifactManager, ArtifactDef artifactDef)
+        {
+            if (artifactDef != ArtifactDef)
+                return;
+            OnArtifactDisabledServer();
+        }
+
+        public bool IsArtifactEnabled()
+        {
+            return RunArtifactManager.instance.IsArtifactEnabled(ArtifactDef);
+        }
+    }
+}

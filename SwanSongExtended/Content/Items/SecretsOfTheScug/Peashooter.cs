a9f2ae8d3d1c189605d76ce2760a08c64d823e6a
Orange_Peel <citrus.shell@gmail.com>
2025-02-21 11:38:40 -0800

    peashooter work

---
+++ SwanSongExtended/Content/Items/SecretsOfTheScug/Peashooter.cs
@@ -0,0 +1,176 @@
+﻿using R2API;
+using RoR2;
+using RoR2.ExpansionManagement;
+using RoR2.Projectile;
+using SwanSongExtended.Modules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace SwanSongExtended.Items
+{
+    class Peashooter : ItemBase<Peashooter>
+    {
+        #region config
+        public override string ConfigName => "Peashooter";
+
+        public static GameObject peashooterPrefab;
+        [AutoConfig("Max Peashooter Fly-Out Time", 0.3f)]
+        public static float maxFlyOutTime = 0.3f; //0.6f
+        [AutoConfig("Peashooter Scale Factor", 0.3f)]
+        public static float peashooterScale = 0.3f; //1.0f
+        [AutoConfig("Peashooter Speed", 50f)]
+        public static float peashooterSpeed = 50f; //1.0f
+
+        [AutoConfig("Damage Coefficient", 5f)]
+        public static float damageCoefficient = 5f;
+        [AutoConfig("Proc Coefficient", 0.8f)]
+        public static float procCoefficient = 0.8f;
+        [AutoConfig("Force", 150f)]
+        public static float force = 150f;
+        #endregion
+        public static BuffDef peashooterBuff;
+        #region abstract
+        public override string ItemName => "Peashooter";
+
+        public override string ItemLangTokenName => "BOOMERANG";
+
+        public override string ItemPickupDesc => "";
+
+        public override string ItemFullDescription => "";
+
+        public override string ItemLore => "";
+
+        public override ItemTier Tier => ItemTier.Tier2;
+
+        public override ItemTag[] ItemTags => new ItemTag[] { ItemTag.Damage};
+
+        public override GameObject ItemModel => LoadDropPrefab();
+
+        public override Sprite ItemIcon => LoadItemIcon();
+        public override ExpansionDef RequiredExpansion => SwanSongPlugin.expansionDefSOTS;
+        #endregion
+
+        public override ItemDisplayRuleDict CreateItemDisplayRules()
+        {
+            return null;
+        }
+
+        public override void Hooks()
+        {
+            On.RoR2.CharacterBody.OnSkillActivated += PeashooterOnSkill;
+            peashooterBuff = Content.CreateAndAddBuff("PeashooterBuff", null, Color.green, true, false);
+            On.RoR2.CharacterBody.OnInventoryChanged += AddItemBehavior;
+        }
+        private void AddItemBehavior(On.RoR2.CharacterBody.orig_OnInventoryChanged orig, RoR2.CharacterBody self)
+        {
+            orig(self);
+            if (NetworkServer.active)
+            {
+                if (self.master)
+                {
+                    PeashooterBehavior peashooterBehavior = self.AddItemBehavior<PeashooterBehavior>(GetCount(self));
+                }
+            }
+        }
+        private void PeashooterOnSkill(On.RoR2.CharacterBody.orig_OnSkillActivated orig, RoR2.CharacterBody self, RoR2.GenericSkill skill)
+        {
+            orig (self, skill);          
+            if (!skill == self.skillLocator.primary)
+            {
+                return;
+            }
+            int peashooterCount = GetCount(self);
+            if (peashooterCount <= 0)
+            {
+                return;
+            }
+            if (self.GetBuffCount(peashooterBuff) >= 1)
+            {
+                FirePeashooter(self);
+            }
+        }
+
+        private void FirePeashooter(CharacterBody body)
+        {
+            body.RemoveBuff(peashooterBuff);
+
+            Vector3 forward = body.inputBank.aimDirection;
+            forward.y = 0;
+          
+
+            ProjectileManager.instance.FireProjectile(new FireProjectileInfo
+            {
+                damage = body.damage * damageCoefficient,
+                crit = body.RollCrit(),
+                damageColorIndex = DamageColorIndex.Item,
+                position = body.corePosition/* + Vector3.up * 3*/,
+                force = force,
+                owner = body.gameObject,
+                projectilePrefab = peashooterPrefab,
+                rotation = Util.QuaternionSafeLookRotation(forward),
+                speedOverride = peashooterSpeed //20
+            });
+        }
+
+        public override void Init()
+        {
+            CreateProjectile();
+            base.Init();
+        }
+        private void CreateProjectile()
+        {
+            peashooterPrefab = LegacyResourcesAPI.Load<GameObject>("RoR2/Base/LunarExploder/LunarExploderShardProjectile.prefab").InstantiateClone("SnailyPeashooter", true);
+            GameObject ghost = LegacyResourcesAPI.Load<GameObject>("RoR2/Base/LunarExploder/LunarExploderShardGhost.prefab ").InstantiateClone("SnailyPeashooterGhost", false);//if this doesnt work and you have to do it the other way:RoR2/Base/Vulture/WindbladeProjectileGhost.prefab
+            peashooterPrefab.transform.localScale = Vector3.one * peashooterScale;
+
+            BoomerangProjectile bp = peashooterPrefab.GetComponent<BoomerangProjectile>();
+            bp.travelSpeed = peashooterSpeed;
+            bp.transitionDuration = 0.8f;
+            bp.distanceMultiplier = maxFlyOutTime;
+            bp.canHitWorld = false;
+
+            ProjectileController pc = bp.GetComponent<ProjectileController>();
+            pc.ghostPrefab = ghost;
+
+            //ProjectileDamage pd = bp.GetComponent<ProjectileDamage>();
+            //pd.damageType |= DamageType.BonusToLowHealth;
+
+            ProjectileDotZone pdz = peashooterPrefab.GetComponent<ProjectileDotZone>();
+            /*pdz.overlapProcCoefficient = 0.8f;
+            pdz.damageCoefficient = 1f;
+            pdz.resetFrequency = 1 / (maxFlyOutTime + bp.transitionDuration);
+            pdz.fireFrequency = 20f;*/
+            UnityEngine.Object.Destroy(pdz);
+
+            ProjectileOverlapAttack poa = peashooterPrefab.GetComponent<ProjectileOverlapAttack>();
+            poa.damageCoefficient = 1f;
+            poa.overlapProcCoefficient = procCoefficient;
+
+            Content.AddProjectilePrefab(peashooterPrefab);
+        }
+    }
+    public class PeashooterBehavior : CharacterBody.ItemBehavior
+    {
+        public static float cooldownDuration = 6;
+        float cooldownTimer = 0;
+        private void FixedUpdate()
+        {
+            if (cooldownTimer > 0)
+            {
+                cooldownTimer -= Time.fixedDeltaTime;
+            }
+            else if (body.GetBuffCount(Peashooter.peashooterBuff) <= 3 + stack--)//make this not hardcoded
+            {
+                RechargeBuff();
+            }
+        }
+        private void RechargeBuff()
+        {
+            body.AddBuff(Peashooter.peashooterBuff);
+            cooldownTimer = cooldownDuration;
+        }
+    }
+}

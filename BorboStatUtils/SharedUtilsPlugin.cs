11e71196debd42105ceb38df78136c7ca9600879
BORBO <bwofodol@gmail.com>
2025-04-26 15:06:25 -0500

    fixed shared utils bug

--- BorboStatUtils/SharedUtilsPlugin.cs
+++ BorboStatUtils/SharedUtilsPlugin.cs
@@ -5,8 +5,15 @@ using R2API;
 using R2API.Utils;
 using RoR2;
 using System;
+using System.Security;
+using System.Security.Permissions;
 using UnityEngine;
 
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 // Type or member is obsolete
+[module: UnverifiableCode]
+#pragma warning disable 
 namespace RainrotSharedUtils
 {
     [BepInDependency(MoreStats.MoreStatsPlugin.guid, BepInDependency.DependencyFlags.HardDependency)]

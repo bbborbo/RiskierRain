46785e30ec731decca01fef443d2918c33884985
BORBO <bwofodol@gmail.com>
2025-05-19 23:49:57 -0500

    fix rare pear wiggler nre

--- SwanSongExtended/Content/Items/SecretsOfTheScug/PearWiggler.cs
+++ SwanSongExtended/Content/Items/SecretsOfTheScug/PearWiggler.cs
@@ -114,7 +114,11 @@ namespace SwanSongExtended.Items
         private void PearWigglerTakeDamage(On.RoR2.HealthComponent.orig_TakeDamageProcess orig, RoR2.HealthComponent self, RoR2.DamageInfo damageInfo)
         {
             orig(self, damageInfo);
-            CharacterBody body = self?.body;
+
+            CharacterBody body = self.body;
+            if (body == null)
+                return;
+
             int itemCount = GetCount(body);
             if (itemCount <= 0)
             {

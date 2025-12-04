8794ea559e7fcbc80ebf28b69acc350e530292ab
Orange_Peel <citrus.shell@gmail.com>
2025-12-03 16:28:07 -0800

    helo everynyan

---
+++ CriticalBlock/Hooks.cs
@@ -0,0 +1,21 @@
+using BepInEx;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using R2API;
+using RoR2;
+using RoR2.Projectile;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+using static R2API.DamageAPI;
+using static R2API.RecalculateStatsAPI;
+
+namespace CriticalBlock
+{
+    public partial  class CriticalBlock : BaseUnityPlugin
+    {
+
+    }
+}

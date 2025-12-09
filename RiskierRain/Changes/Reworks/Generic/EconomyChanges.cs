96362d7345e7e25fdffe866f946eb71c36a0b05e
BORBO <bwofodol@gmail.com>
2025-12-09 17:53:55 -0600

    increased gold drops by 1

--- RiskierRain/Changes/Reworks/Generic/EconomyChanges.cs
+++ RiskierRain/Changes/Reworks/Generic/EconomyChanges.cs
@@ -226,7 +226,7 @@ namespace RiskierRain
             c.EmitDelegate<Func<uint, uint>>((money) =>
             {
                 float compensated = GetCompensatedDifficultyFraction();
-                return (uint)Mathf.CeilToInt(money * compensated * goldRewardMultiplierGlobal);
+                return (uint)Mathf.CeilToInt(money * compensated * goldRewardMultiplierGlobal) + 1;
             });
             c.Emit(OpCodes.Starg, 1);
         }

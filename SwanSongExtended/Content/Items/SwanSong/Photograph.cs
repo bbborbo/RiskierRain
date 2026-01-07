1bb0ccd2ca649a488fe0bc1fb6ea366f7cdf6cda
BORBO <bwofodol@gmail.com>
2026-01-07 02:17:54 -0600

    added photograph

---
+++ SwanSongExtended/Content/Items/SwanSong/Photograph.cs
@@ -0,0 +1,105 @@
+﻿using On.RoR2.Items;
+using R2API;
+using RoR2;
+using SwanSongExtended.Modules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using static R2API.RecalculateStatsAPI;
+
+namespace SwanSongExtended.Items
+{
+    class Photograph : ItemBase<Photograph>
+    {
+        public static BuffDef photographCritBuff;
+        public static float photographCritFreeBase = 0f;
+        public static float photographCritFreeStack = 0f;
+        public static float photographCritBase = 15f;
+        public static float photographCritStack = 15f;
+        public override string ItemName => "Photograph";
+
+        public override string ItemLangTokenName => "PHOTOGRAPH";
+
+        public override string ItemPickupDesc => "Printing items increases critical strike chance and damage. Resets each stage.";
+
+        public override string ItemFullDescription => ItemPickupDesc;
+
+        public override string ItemLore => $"Did you get your photos printed?\n\n\"Bogos binted?\"\n\nHuh?\n\n\"Download GreenAlienHead\"";
+
+        public override ItemTier Tier => ItemTier.Tier1;
+
+        public override ItemTag[] ItemTags => new ItemTag[] { ItemTag.Damage, ItemTag.DevotionBlacklist, ItemTag.InteractableRelated };
+
+        public override GameObject ItemModel => LoadDropPrefab();
+
+        public override Sprite ItemIcon => LoadItemIcon();
+
+        public override ItemDisplayRuleDict CreateItemDisplayRules()
+        {
+            return new ItemDisplayRuleDict();
+        }
+
+        public override void Init()
+        {
+            photographCritBuff = Content.CreateAndAddBuff(
+                "bdPhotographCrit",
+                null,
+                Color.red,
+                true,
+                false,
+                BuffDef.StackingDisplayMethod.Percentage
+                );
+            base.Init();
+        }
+
+        public override void Hooks()
+        {
+            On.RoR2.Items.MultiShopCardUtils.OnNonMoneyPurchase += PhotographOnNonMoneyPurchase;
+            GetStatCoefficients += PhotographCritBonus;
+        }
+
+        private void PhotographCritBonus(CharacterBody sender, StatHookEventArgs args)
+        {
+            int itemCount = GetCount(sender);
+            if (itemCount > 0)
+            {
+                args.critAdd += photographCritFreeBase + (photographCritFreeStack * (itemCount - 1));
+                args.critDamageMultAdd += photographCritFreeBase + (photographCritFreeStack * (itemCount - 1));
+            }
+            int buffCount = sender.GetBuffCount(photographCritBuff);
+            if(buffCount > 0)
+            {
+                args.critAdd += buffCount;
+                args.critDamageMultAdd += buffCount;
+            }
+        }
+
+        private void PhotographOnNonMoneyPurchase(MultiShopCardUtils.orig_OnNonMoneyPurchase orig, CostTypeDef.PayCostContext context)
+        {
+            PhotographOnPrinterPurchase(context);
+            orig(context);
+        }
+
+        private void PhotographOnPrinterPurchase(CostTypeDef.PayCostContext context)
+        {
+            if (context.costTypeDef != CostTypeCatalog.GetCostTypeDef(CostTypeIndex.WhiteItem)
+                    && context.costTypeDef != CostTypeCatalog.GetCostTypeDef(CostTypeIndex.GreenItem)
+                    && context.costTypeDef != CostTypeCatalog.GetCostTypeDef(CostTypeIndex.RedItem)
+                    && context.costTypeDef != CostTypeCatalog.GetCostTypeDef(CostTypeIndex.BossItem)
+                    && context.costTypeDef != CostTypeCatalog.GetCostTypeDef(CostTypeIndex.LunarItemOrEquipment)
+                    )
+                return;
+
+            int itemCount = GetCount(context.activatorInventory);
+            if (itemCount <= 0)
+                return;
+
+            float critBonus = photographCritBase + (photographCritFreeStack * (itemCount - 1));
+            for(int i = 0; i < critBonus * 100; i++)
+            {
+                context.activatorBody.AddBuff(photographCritBuff);
+            }
+        }
+    }
+}

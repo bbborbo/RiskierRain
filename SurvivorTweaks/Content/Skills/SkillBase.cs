330822458b4f7a65a8755cb294b572967e542a59
BORBO <bwofodol@gmail.com>
2026-01-08 16:59:39 -0600

    fix fst bonus stocks

--- SurvivorTweaks/Content/Skills/SkillBase.cs
+++ SurvivorTweaks/Content/Skills/SkillBase.cs
@@ -304,7 +304,7 @@ namespace SurvivorTweaks.Skills
         {
             public SimpleSkillData(int baseMaxStock = 1, bool beginSkillCooldownOnSkillEnd = false,
                 bool canceledFromSprinting = false, bool cancelSprintingOnActivation = true, bool forceSprintingDuringState = false,
-                bool dontAllowPastMaxStocks = true, bool fullRestockOnAssign = true,
+                bool dontAllowPastMaxStocks = false, bool fullRestockOnAssign = true,
                 bool isCombatSkill = true, bool mustKeyPress = false, int rechargeStock = 1,
                 int requiredStock = 1, bool resetCooldownTimerOnUse = false, int stockToConsume = 1,
                 bool useAttackSpeedScaling = false)

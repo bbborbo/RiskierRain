b86d06302a1e4696b9e3ced3a7c7761f15a0f2d8
BORBO <bwofodol@gmail.com>
2026-01-20 18:53:58 -0600

    attempted fruitycradles compat fix with quality

--- FruityCradles/FruityCradlesPlugin.cs
+++ FruityCradles/FruityCradlesPlugin.cs
@@ -16,6 +16,7 @@ namespace FruityCradles
     //[BepInDependency(R2API.RecalculateStatsAPI.PluginGUID, BepInDependency.DependencyFlags.HardDependency)]
     //[BepInDependency(MoreStatsPlugin.guid, BepInDependency.DependencyFlags.HardDependency)]
     [BepInDependency(SoulCostPlugin.guid, BepInDependency.DependencyFlags.SoftDependency)]
+    [BepInDependency("com.Gorakh.ItemQualities", BepInDependency.DependencyFlags.SoftDependency)]
 
     [BepInPlugin(guid, modName, version)]
     //[R2APISubmoduleDependency(nameof(LanguageAPI), nameof(ContentAddition), nameof(DamageAPI))]

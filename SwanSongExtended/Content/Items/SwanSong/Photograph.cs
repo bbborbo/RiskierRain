2fc447f14f7a4c1ca712d973f365eb9f4d8f5bbf
BORBO <bwofodol@gmail.com>
2026-01-22 14:35:21 -0600

    nerf photograph

--- SwanSongExtended/Content/Items/SwanSong/Photograph.cs
+++ SwanSongExtended/Content/Items/SwanSong/Photograph.cs
@@ -17,10 +17,10 @@ namespace SwanSongExtended.Items
         public static BuffDef photographCritBuff;
         public static float photographCritFreeBase = 0f;
         public static float photographCritFreeStack = 0f;
-        public static float photographCritBase = 15f;
-        public static float photographCritStack = 10f;
-        public static int photographMaxPrintsBase = 2;
-        public static int photographMaxPrintsStack = 1;
+        public static float photographCritBase = 12f;
+        public static float photographCritStack = 8f;
+        public static int photographMaxPrintsBase = 3;
+        public static int photographMaxPrintsStack = 0;
         public override string ItemName => "Photograph";
 
         public override string ItemLangTokenName => "PHOTOGRAPH";

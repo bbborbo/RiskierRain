1f57f4ac9e1d0b594f65129345627f1c1e74d1cc
BORBO <bwofodol@gmail.com>
2026-02-14 15:14:46 -0600

    meteor delayblast

--- SwanSongExtended/Storms/StormsCore.cs
+++ SwanSongExtended/Storms/StormsCore.cs
@@ -46,8 +46,9 @@ namespace SwanSongExtended.Storms
         public const float stormStrengthIncreaseBase = 0.1f;
 
         //meteors:
-        public static GameObject meteorWarningEffectPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Meteor/MeteorStrikePredictionEffect.prefab").WaitForCompletion();
-        public static GameObject meteorImpactEffectPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Meteor/MeteorStrikeImpact.prefab").WaitForCompletion();
+        public static GameObject meteorDelayBlastPrefab;
+        public static GameObject meteorWarningEffectPrefab = Addressables.LoadAssetAsync<GameObject>(RoR2BepInExPack.GameAssetPathsBetter.RoR2_Base_Meteor.MeteorStrikePredictionEffect_prefab).WaitForCompletion();
+        public static GameObject meteorImpactEffectPrefab = Addressables.LoadAssetAsync<GameObject>(RoR2BepInExPack.GameAssetPathsBetter.RoR2_Base_Meteor.MeteorStrikeImpact_prefab).WaitForCompletion();
         public static float waveMinInterval = 0.7f;
         public static float waveMaxInterval = 1.0f;
         public static float waveMissChance = 0.6f;
@@ -76,40 +77,56 @@ namespace SwanSongExtended.Storms
             //On.RoR2.HoldoutZoneController.OnEnable += RegisterHoldoutZone;
             //On.RoR2.HoldoutZoneController.OnDisable += UnregisterHoldoutZone;
 
-            meteorWarningEffectPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Meteor/MeteorStrikePredictionEffect.prefab").WaitForCompletion().InstantiateClone("StormStrikePredictionEffect");
-            meteorWarningEffectPrefab.transform.localScale =  new Vector3(meteorBlastRadius * 0.85f, meteorBlastRadius * 5, meteorBlastRadius * 0.85f);
-            DestroyOnTimer DOT = meteorWarningEffectPrefab.GetComponent<DestroyOnTimer>();
-            if (DOT)
-            {
-                DOT.duration = meteorImpactDelay + 0.5f;
-            }
-            Transform indicator = meteorWarningEffectPrefab.transform.Find("GroundSlamIndicator");
-            if (indicator)
-            {
-                AnimateShaderAlpha asa = indicator.GetComponent<AnimateShaderAlpha>();
-                if (asa)
-                {
-                    asa.timeMax = meteorImpactDelay + 0.1f;
-                }
-                MeshRenderer meshRenderer = indicator.GetComponent<MeshRenderer>();
-                if (meshRenderer)
-                {
-                    Material mat = UnityEngine.Object.Instantiate(meshRenderer.material);
-                    mat.name = "matStormStrikeImpactIndicator";
-                    meshRenderer.material = mat;
-                    mat.SetFloat("_Boost", 0.64f);
-                    mat.SetFloat("_AlphaBoost", 4.29f);
-                    mat.SetTexture("_RemapTex", Addressables.LoadAssetAsync<Texture>("RoR2/Base/Common/ColorRamps/texRampArtifactShellSoft.png").WaitForCompletion());
-                    mat.SetColor("_TintColor", Color.white);
-                }
-            }
-            Content.CreateAndAddEffectDef(meteorWarningEffectPrefab);
-
+            SwanSongPlugin.LoadAsync<GameObject>(RoR2BepInExPack.GameAssetPathsBetter.RoR2_Base_Common.GenericDelayBlast_prefab, CreateMeteorDelayBlast);
+            
             LanguageAPI.Add($"OBJECTIVE_METEORDEFAULT_2R4R", "Meteor Storm Imminent");
             LanguageAPI.Add($"OBJECTIVE_LIGHTNING_2R4R", "Thunderstorm Imminent");
             LanguageAPI.Add($"OBJECTIVE_FIRE_2R4R", "Fire Storm Imminent");
             LanguageAPI.Add($"OBJECTIVE_COLD_2R4R", "Blizzard Imminent");
             //LanguageAPI.Add($"OBJECTIVE_METEORDEFAULT_2R4R", "");
+        }
+
+        private static void CreateMeteorDelayBlast(GameObject delayBlastPrefab)
+        {
+            meteorDelayBlastPrefab = delayBlastPrefab.InstantiateClone("StormStrikeDelayBlastProjectile", true);
+
+            if(meteorDelayBlastPrefab.TryGetComponent(out NetworkIdentity netId))
+            {
+                netId.localPlayerAuthority = true;
+            }
+
+            SwanSongPlugin.LoadAsync<GameObject>(RoR2BepInExPack.GameAssetPathsBetter.RoR2_Base_Meteor.MeteorStrikePredictionEffect_prefab, (predictionEffect) =>
+            {
+                meteorWarningEffectPrefab = predictionEffect.InstantiateClone("StormStrikePredictionEffect");
+                meteorWarningEffectPrefab.transform.localScale = new Vector3(meteorBlastRadius * 0.85f, meteorBlastRadius * 5, meteorBlastRadius * 0.85f);
+                DestroyOnTimer DOT = meteorWarningEffectPrefab.GetComponent<DestroyOnTimer>();
+                if (DOT)
+                {
+                    DOT.duration = meteorImpactDelay + 1f;
+                }
+                Transform indicator = meteorWarningEffectPrefab.transform.Find("GroundSlamIndicator");
+                if (indicator)
+                {
+                    AnimateShaderAlpha asa = indicator.GetComponent<AnimateShaderAlpha>();
+                    if (asa)
+                    {
+                        asa.timeMax = meteorImpactDelay + 0.1f;
+                    }
+                    MeshRenderer meshRenderer = indicator.GetComponent<MeshRenderer>();
+                    if (meshRenderer)
+                    {
+                        Material mat = UnityEngine.Object.Instantiate(meshRenderer.material);
+                        mat.name = "matStormStrikeImpactIndicator";
+                        meshRenderer.material = mat;
+                        mat.SetFloat("_Boost", 0.64f);
+                        mat.SetFloat("_AlphaBoost", 4.29f);
+                        mat.SetTexture("_RemapTex", Addressables.LoadAssetAsync<Texture>("RoR2/Base/Common/ColorRamps/texRampArtifactShellSoft.png").WaitForCompletion());
+                        mat.SetColor("_TintColor", Color.white);
+                    }
+                }
+
+                Content.CreateAndAddEffectDef(meteorWarningEffectPrefab);
+            });
         }
 
         private static void AddDifficultyStats()

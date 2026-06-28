fe6fd7afc9fac37c848fd50178cfe68a51f1d374
Orange_Peel <citrus.shell@gmail.com>
2026-06-28 14:36:32 -0700

    chicken rework

--- SwanSongExtended/Content/Items/SwanSong/Chicken.cs
+++ SwanSongExtended/Content/Items/SwanSong/Chicken.cs
@@ -2,6 +2,7 @@
 using R2API;
 using RoR2;
 using RoR2.ExpansionManagement;
+using RoR2.Items;
 using SwanSongExtended.Modules;
 using System;
 using System.Collections.Generic;
@@ -17,22 +18,19 @@ namespace SwanSongExtended.Items
     {
         public static BuffDef foodPoisoning;
         public static float baseMaxHealth = 0.3f;
-        public static float stackMaxHealth = 0.2f;
+        public static float stackMaxHealth = 0.3f;
         public static float baseRegenPenalty = 2f;
-        public static float stackRegenPenalty = 2f;
-        public static float regenPenaltyChance = 40f;
         public override ExpansionDef RequiredExpansion => SwanSongPlugin.expansionDefSS2;
         public override string ItemName => "Raw Chicken";
 
         public override string ItemLangTokenName => "CHICKEN";
 
-        public override string ItemPickupDesc => "Substantially increase max health, at risk of food poisoning.";
+        public override string ItemPickupDesc => "Increases max health. Try to keep it down!";
 
         public override string ItemFullDescription => $"Increase your <style=cIsHealing>maximum health</style> by " +
             $"<style=cIsHealing>{Tools.ConvertDecimal(baseMaxHealth)}</style> <style=cStack>(+{Tools.ConvertDecimal(stackMaxHealth)} per stack)</style>. " +
-            $"At the start of each stage, has a <style=cIsHealing>{regenPenaltyChance}%</style> chance to inflict " +
-            $"<style=cIsHealing>food poisoning</style>, reducing your <style=cIsHealing>base health regeneration</style> " +
-            $"by <style=cIsHealing>-{baseRegenPenalty} hp/s</style> <style=cStack>(-{baseRegenPenalty} hp/s per stack)</style>.";
+            $"<style=cIsHealing>Suffer food poisoning</style>, reducing your <style=cIsHealing>base health regeneration</style> " +
+            $"by <style=cIsHealing>-{baseRegenPenalty} hp/s</style> for 120 seconds.";
 
         public override string ItemLore => "";
 
@@ -61,37 +59,6 @@ namespace SwanSongExtended.Items
         public override void Hooks()
         {
             GetStatCoefficients += ChickenStats;
-            CharacterBody.onBodyStartGlobal += DoFoodPoisoning;
-        }
-
-        private void DoFoodPoisoning(CharacterBody body)
-        {
-            if (!NetworkServer.active)
-                return;
-            int chickenCount = GetCount(body);
-            if (chickenCount <= 0)
-                return;
-
-            float regenPenaltyChance = GetPoisonChance(body);
-
-            if (!Util.CheckRoll(100 - regenPenaltyChance, body.master))
-            {
-                for (int i = 0; i < chickenCount; i++)
-                {
-                    body.AddTimedBuff(Chicken.foodPoisoning.buffIndex, 120);
-                }
-            }
-
-            float GetPoisonChance(CharacterBody body)
-            {
-                if (body.bodyIndex == BodyCatalog.FindBodyIndex("FalseSonBody"))
-                    return 100;
-                if (Run.instance.stageClearCount < 1)
-                    return Math.Min(0.01f, Chicken.regenPenaltyChance);
-                if (Run.instance.stageClearCount < 2)
-                    return Chicken.regenPenaltyChance / 2;
-                return Chicken.regenPenaltyChance;
-            }
         }
 
         private void ChickenStats(CharacterBody sender, StatHookEventArgs args)
@@ -101,7 +68,18 @@ namespace SwanSongExtended.Items
                 args.healthMultAdd += baseMaxHealth + stackMaxHealth * (chickenCount - 1);
             int poisonCount = sender.GetBuffCount(foodPoisoning);
             if (poisonCount > 0)
-                args.baseRegenAdd -= (baseRegenPenalty + stackRegenPenalty * (poisonCount - 1)) * (1 + 0.2f * sender.level);
+                args.baseRegenAdd -= (baseRegenPenalty * (poisonCount - 1)) * (1 + 0.2f * sender.level);
+        }
+    }
+    public class RawChickenBehavior : BaseItemBodyBehavior
+    {
+        [ItemDefAssociation(useOnServer = true, useOnClient = false)]
+        private static ItemDef GetItemDef() => Chicken.instance.ItemsDef;
+        public int duration = 120;
+
+        private void Start()
+        {
+            body.AddTimedBuff(Chicken.foodPoisoning, duration);
         }
     }
 }

894b189163a91553143b848dcc0cef5809779d9d
BORBO <bwofodol@gmail.com>
2026-08-28 08:46:28 -0500

    delay enemy shield recharge when teleport/invincible

--- EliteReworks/Modules/Hooks.cs
+++ EliteReworks/Modules/Hooks.cs
@@ -12,6 +12,44 @@ namespace FruityElites.Modules
         public static void Init()
         {
             MakeSpawnSlotSpawnsInheritEliteAffix();
+            On.RoR2.CharacterBody.FixedUpdate += CharacterBodyDelayOutOfCombat;
+            On.RoR2.TeleportHelper.TeleportBody_TeleportBodyArgs += TeleportBodyDelayOutOfCombat;
+        }
+
+        private static void TeleportBodyDelayOutOfCombat(On.RoR2.TeleportHelper.orig_TeleportBody_TeleportBodyArgs orig, TeleportHelper.TeleportBodyArgs bodyArgs)
+        {
+            orig(bodyArgs);
+            DelayOutOfCombat(bodyArgs.body);
+        }
+
+        private static void CharacterBodyDelayOutOfCombat(On.RoR2.CharacterBody.orig_FixedUpdate orig, CharacterBody self)
+        {
+            orig(self);
+
+            if (GetInvincible(self))
+            {
+                DelayOutOfCombat(self, self.outOfDangerStopwatch - Time.fixedDeltaTime);
+            }
+
+            bool GetInvincible(CharacterBody self)
+            {
+                return 
+                    self.HasBuff(RoR2Content.Buffs.HiddenInvincibility) 
+                    || self.HasBuff(RoR2Content.Buffs.Immune) 
+                    || self.HasBuff(RoR2Content.Buffs.Intangible) 
+                    || self.HasBuff(DLC2Content.Buffs.HiddenRejectAllDamage)
+                    //|| self.HasBuff(RoR2Content.Buffs.Cloak)
+                    //|| (self.TryGetComponent(out CharacterModel model) == true && model.invisibilityCount > 0)
+                    ;
+            }
+        }
+
+        private static void DelayOutOfCombat(CharacterBody self, float stopwatch = 0)
+        {
+            if (self.isPlayerControlled)
+                return;
+            self.outOfDangerStopwatch = Mathf.Clamp(stopwatch, 0, 5f);
+            self.outOfDanger = false;
         }
 
         static void MakeSpawnSlotSpawnsInheritEliteAffix()

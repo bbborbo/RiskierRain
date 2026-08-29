91a08b959a90287df8b262e3a404744ba9ac97f2
BORBO <bwofodol@gmail.com>
2026-08-29 14:08:08 -0500

    finish blazing rework

--- EliteReworks/Content/EliteReworks/BlazingReworks.cs
+++ EliteReworks/Content/EliteReworks/BlazingReworks.cs
@@ -8,6 +8,10 @@ using UnityEngine.Networking;
 using static R2API.RecalculateStatsAPI;
 using static MoreStats.StatHooks;
 using HG;
+using R2API;
+using UnityEngine.AddressableAssets;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace FruityElites.EliteReworks
 {
@@ -26,7 +30,7 @@ namespace FruityElites.EliteReworks
         public static float flameAuraGrowthPerSecond = 0.25f;
         public static float flameAuraDamageInterval = 0.5f;
 
-        public static float flameAuraIgniteTotalDamageBase = 15f;
+        public static float flameAuraIgniteTotalDamageBase = 7f;
         public static float flameAuraIgniteTotalDamageLevel = 0.4f;
 
 
@@ -41,7 +45,96 @@ namespace FruityElites.EliteReworks
         public override void Init()
         {
             accelerantBuff = Content.CreateAndAddBuff("bdBlazingAccelerant", null, Color.red, false, false);
+            EliteReworksPlugin.LoadAsync<BuffDef>(RoR2BepInExPack.GameAssetPaths.Version_1_39_0.RoR2_Base_Common.bdOnFire_asset, (onFire) =>
+            {
+                accelerantBuff.iconSprite = onFire.iconSprite;
+            });
+            EliteReworksPlugin.LoadAsync<GameObject>(RoR2BepInExPack.GameAssetPaths.Version_1_39_0.RoR2_Base_BurnNearby.HelfireController_prefab, CreateFlameAura);
+            EliteReworksPlugin.LoadAsync<GameObject>(RoR2BepInExPack.GameAssetPaths.Version_1_39_0.RoR2_Base_NearbyDamageBonus.NearbyDamageBonusIndicator_prefab, CreateRangeIndicator);
             base.Init();
+        }
+
+        private void CreateRangeIndicator(GameObject obj)
+        {
+            flameAuraMaxRangeIndicatorPrefab = obj.InstantiateClone("BlazingRangeIndicator", true);
+
+            Transform radiusSpherical = flameAuraMaxRangeIndicatorPrefab.transform.GetChild(1);
+            if (radiusSpherical)
+            {
+                radiusSpherical.transform.localScale = Vector3.one;
+
+                if (radiusSpherical.gameObject.TryGetComponent(out MeshRenderer meshRenderer))
+                {
+                    Material mat = UnityEngine.Object.Instantiate(meshRenderer.material);
+                    mat.SetColor("_TintColor", new Color32(255, 118, 24, 192));
+
+                    meshRenderer.material = mat;
+                }
+            }
+
+            Modules.Content.AddNetworkedObjectPrefab(flameAuraMaxRangeIndicatorPrefab);
+        }
+
+        private void CreateFlameAura(GameObject helfireAura)
+        {
+            Transform auraTransform = helfireAura.transform.Find("AuraTransform");
+            if(auraTransform == null)
+            {
+                if(helfireAura.TryGetComponent(out HelfireController helfireController))
+                {
+                    auraTransform = helfireController.auraEffectTransform;
+                }
+            }
+            if(auraTransform == null)
+            {
+                Log.Error("FruityElites could not get blazing aura effect");
+                return;
+            }
+
+            flameAuraPrefab = auraTransform.gameObject.InstantiateClone("BlazingFlameAura", false);
+
+            Material matAura;
+            Transform radiusSpherical = flameAuraPrefab.transform.GetChild(1);
+            if (radiusSpherical != null && radiusSpherical.gameObject.TryGetComponent(out MeshRenderer meshRenderer))
+            {
+                matAura = UnityEngine.Object.Instantiate(meshRenderer.material);
+                matAura.SetColor("_TintColor", Color.white);
+                matAura.SetTexture("_RemapTex", Addressables.LoadAssetAsync<Texture>(RoR2BepInExPack.GameAssetPathsBetter.RoR2_Base_Common_ColorRamps.texRampMagmaWorm_png).WaitForCompletion());
+                matAura.SetFloat("_RimPower", 3.98f);
+                matAura.SetFloat("_RimStrength", 0.61f);
+                meshRenderer.material = matAura;
+            }
+
+            Material matParticle;
+            Transform worldFire = flameAuraPrefab.transform.GetChild(0);
+            worldFire.transform.localScale = Vector3.one * 3;
+            if (worldFire.gameObject.TryGetComponent(out ParticleSystemRenderer psr1))
+            {
+                matParticle = UnityEngine.Object.Instantiate(psr1.material);
+                matParticle.SetColor("_TintColor", Color.white);
+                matParticle.SetTexture("_RemapTex", Addressables.LoadAssetAsync<Texture>(RoR2BepInExPack.GameAssetPathsBetter.RoR2_Base_Common_ColorRamps.texRampMageFire_png).WaitForCompletion());
+                matParticle.SetFloat("_Boost", 3.32f); //0.34
+                psr1.material = matParticle;
+
+                Transform localFire = flameAuraPrefab.transform.GetChild(2);
+                localFire.transform.localScale = Vector3.one * 2;
+                if (localFire.gameObject.TryGetComponent(out ParticleSystemRenderer psr2))
+                {
+                    psr2.material = matParticle;
+                }
+                Transform localFireSpherical = flameAuraPrefab.transform.GetChild(3);
+                localFireSpherical.transform.localScale = Vector3.one * 2;
+                if (localFireSpherical.gameObject.TryGetComponent(out ParticleSystemRenderer psr3))
+                {
+                    psr3.material = matParticle;
+                }
+                Transform spit = flameAuraPrefab.transform.GetChild(4);
+                spit.transform.localScale = Vector3.one * 2;
+                if (spit.gameObject.TryGetComponent(out ParticleSystemRenderer psr4))
+                {
+                    psr4.material = matParticle;
+                }
+            }
         }
 
         public override void Hooks()
@@ -81,7 +174,7 @@ namespace FruityElites.EliteReworks
 
         private void BlazingFireTrailChanges(On.RoR2.CharacterBody.orig_UpdateFireTrail orig, CharacterBody self)
         {
-            orig(self);
+
             return;
 
             if (self.fireTrail)
@@ -94,9 +187,13 @@ namespace FruityElites.EliteReworks
     }
     public class AffixRedBehavior : CharacterBody.ItemBehavior, IOnTakeDamageServerReceiver, IOnKilledServerReceiver
     {
+        private static List<AffixRedBehavior> instancesList = new List<AffixRedBehavior>();
+        public static ReadOnlyCollection<AffixRedBehavior> readOnlyInstancesList = new ReadOnlyCollection<AffixRedBehavior>(AffixRedBehavior.instancesList);
+
         float flameAuraMaxRange => body.bestFitRadius + BlazingReworks.flameAuraRange;
+        float flameAuraGrowthPerSecond => flameAuraMaxRange * BlazingReworks.flameAuraGrowthPerSecond;
         float flameAuraDamageInterval => BlazingReworks.flameAuraDamageInterval / body.attackSpeed;
-        private const float rangeIndicatorScale = 1;
+        private const float rangeIndicatorScale = 2;
         private const float auraScale = 1;
         private const float minRange = 2;
         private float currentRange = 0;
@@ -106,15 +203,38 @@ namespace FruityElites.EliteReworks
 
         SphereSearch sphereSearch;
 
+        private bool indicatorEnabled
+        {
+            get
+            {
+                return this.rangeIndicatorInstance != null;
+            }
+            set
+            {
+                if (this.indicatorEnabled == value)
+                {
+                    return;
+                }
+                if (value)
+                {
+                    this.rangeIndicatorInstance = UnityEngine.Object.Instantiate<GameObject>(BlazingReworks.flameAuraMaxRangeIndicatorPrefab, base.body.corePosition, Quaternion.identity);
+                    this.rangeIndicatorInstance.transform.localScale = Vector3.one * rangeIndicatorScale * flameAuraMaxRange;
+                    this.rangeIndicatorInstance.GetComponent<NetworkedBodyAttachment>().AttachToGameObjectAndSpawn(base.gameObject, null);
+                    return;
+                }
+                UnityEngine.Object.Destroy(this.rangeIndicatorInstance);
+                this.rangeIndicatorInstance = null;
+            }
+        }
+
         void Start()
         {
+            instancesList.Add(this);
             body?.healthComponent?.AddOnTakeDamageServerReceiver(this);
 
+            indicatorEnabled = true;
             auraInstance = Instantiate(BlazingReworks.flameAuraPrefab, body.transform);
             SetAuraRange(0);
-
-            rangeIndicatorInstance = Instantiate(BlazingReworks.flameAuraMaxRangeIndicatorPrefab, body.transform);
-            rangeIndicatorInstance.transform.localScale = Vector3.one * flameAuraMaxRange * rangeIndicatorScale;
 
             if (NetworkServer.active)
             {
@@ -127,14 +247,32 @@ namespace FruityElites.EliteReworks
         }
         void OnDestroy()
         {
+            if (instancesList.Contains(this))
+            {
+                instancesList.Remove(this);
+            }
             body?.healthComponent?.RemoveOnTakeDamageServerReceiver(this);
             Destroy(auraInstance);
-            Destroy(rangeIndicatorInstance);
+            indicatorEnabled = false;
+            //if (body.healthComponent.alive == false)
+            //    AccelerateNearby(TeamIndex.Player);
         }
         public void OnTakeDamageServer(DamageReport damageReport)
         {
             if(damageReport.damageInfo.damageType.IsDamageSourceSkillBased || damageReport.damageInfo.damageType.damageSource == DamageSource.Equipment)
-                ResetFlameAura();
+            {
+                List<AffixRedBehavior> reds = new List<AffixRedBehavior>(readOnlyInstancesList).Where(x => 
+                x.currentRange > minRange
+                && ((body.corePosition - x.body.corePosition).sqrMagnitude <= flameAuraMaxRange * flameAuraMaxRange)
+                ).ToList();
+
+                foreach(AffixRedBehavior red in reds)
+                    red.ResetFlameAura();
+            }
+            //if(body.healthComponent.alive == false && body.healthComponent.wasAlive == true && damageReport.attackerTeamIndex != damageReport.victimTeamIndex)
+            //{
+            //    AccelerateNearby(damageReport.attackerTeamIndex);
+            //}
         }
 
         void Update()
@@ -144,7 +282,7 @@ namespace FruityElites.EliteReworks
                 if (currentRange < minRange)
                     damageStopwatch = flameAuraDamageInterval;
                 if (currentRange < flameAuraMaxRange)
-                    SetAuraRange(MathF.Min(flameAuraMaxRange, currentRange + BlazingReworks.flameAuraGrowthPerSecond * Time.deltaTime));
+                    SetAuraRange(MathF.Min(flameAuraMaxRange, currentRange + flameAuraGrowthPerSecond * Time.deltaTime));
             }
         }
 
@@ -185,12 +323,17 @@ namespace FruityElites.EliteReworks
 
         public void OnKilledServer(DamageReport damageReport)
         {
+            AccelerateNearby(damageReport.attackerTeamIndex);
+        }
+        void AccelerateNearby(TeamIndex targetTeam)
+        {
             //get targets
-            List<HurtBox> enemies = GetNearbyTargets(flameAuraMaxRange, damageReport.attackerTeamIndex, false);
+            List<HurtBox> enemies = GetNearbyTargets(flameAuraMaxRange, targetTeam, false);
 
             //buff targets/send buff orb
             foreach (HurtBox target in enemies)
             {
+                CharacterBody body = target.healthComponent.body;
                 target.healthComponent.body.AddTimedBuff(BlazingReworks.accelerantBuff, BlazingReworks.accelerantDuration);
             }
         }
@@ -199,7 +342,7 @@ namespace FruityElites.EliteReworks
         {
             List<HurtBox> candidates = new List<HurtBox>();
             sphereSearch.origin = this.transform.position;
-            sphereSearch.radius = this.currentRange;
+            sphereSearch.radius = radius;
 
             TeamMask mask = default(TeamMask);
             if (invertTeam)
@@ -222,14 +365,16 @@ namespace FruityElites.EliteReworks
             }
             if (shouldBeActive)
             {
-                auraInstance.transform.localScale = Vector3.one * rangeIndicatorScale * currentRange;
+                auraInstance.transform.localScale = Vector3.one * auraScale * currentRange;
             }
         }
 
-        void ResetFlameAura()
+        public void ResetFlameAura()
         {
             SetAuraRange(0);
             damageStopwatch = flameAuraDamageInterval;
+            //body.outOfCombatStopwatch = 0;
+            //body.outOfDanger = false;
         }
     }
 }

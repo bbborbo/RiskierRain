19d4fffafb32178899d21147396b5fdfaca857b3
BORBO <bwofodol@gmail.com>
2026-09-06 22:23:28 -0500

    fix drone changes

--- RiskierRain/Changes/Allies/AllyChanges.cs
+++ RiskierRain/Changes/Allies/AllyChanges.cs
@@ -15,6 +15,7 @@ namespace RiskierRain.Changes
             ChangeMegaDrone();
         }
 
+        #region tc-280 prototype drone mega drone
         public static float megaDroneBaseMaxHealth = 450f;//1200
         public static float megaDroneBaseDamage = 10f;//14
         private static void ChangeMegaDrone()
@@ -27,22 +28,41 @@ namespace RiskierRain.Changes
                     body.levelMaxHealth = body.baseMaxHealth * 0.3f;
                     body.baseDamage = megaDroneBaseDamage;
                     body.levelDamage = body.baseDamage * 0.2f;
+                    body.PerformAutoCalculateLevelStats();
                 }
             });
         }
+        #endregion
 
+        #region junk drone
+        public static float junkDroneDropCoefficient = 1000;//1?
+        public static int junkDroneDropsMax = 3;//5
+        public static int junkDroneDropsPerTier = 1;//1
         private static void ChangeJunkDrone()
         {
-            EntityStates.Drone.DroneJunk.Surprise.itemsToDropCoefficient = 1000;
-            EntityStates.Drone.DroneJunk.Surprise.maxItemCount = 3;
-            EntityStates.Drone.DroneJunk.Surprise.extraItemsPerTier = 2;
+            On.EntityStates.Drone.DroneJunk.Surprise.OnEnter += (orig, self) =>
+            {
+                EntityStates.Drone.DroneJunk.Surprise.itemsToDropCoefficient = junkDroneDropCoefficient;
+                EntityStates.Drone.DroneJunk.Surprise.maxItemCount = junkDroneDropsMax;
+                EntityStates.Drone.DroneJunk.Surprise.extraItemsPerTier = junkDroneDropsPerTier;
+            };
         }
+        #endregion
 
+        #region cleanup drone
+        public static int cleanupValueGold = 8; //12
+        public static float cleanupValueHealFraction = 0.10f;//0.15f
+        public static float cleanupValueHealFlat = 0f;//0f
         private static void ChangeCleanupDrone()
         {
-            EntityStates.Drone.Cleanup.goldPackValue = 8;
-            EntityStates.Drone.Cleanup.healthOrbFractionalHealing = 0.10f;
-            EntityStates.Drone.Cleanup.healthOrbFlatHealing = 0;
+            On.EntityStates.Drone.Cleanup.OnEnter += (orig, self) =>
+            {
+                EntityStates.Drone.Cleanup.goldPackValue = cleanupValueGold;
+                EntityStates.Drone.Cleanup.healthOrbFractionalHealing = cleanupValueHealFraction;
+                EntityStates.Drone.Cleanup.healthOrbFlatHealing = cleanupValueHealFlat;
+                orig(self);
+            };
         }
+        #endregion
     }
 }
